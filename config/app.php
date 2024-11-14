fbef97f1c44660e6733a9643710881f2f907302b
xiekunyu <raingad@foxmail.com>
2024-11-14 21:17:40 +0800

    更新版本和前端

--- config/app.php
+++ config/app.php
@@ -6,8 +6,8 @@
 return [
     'app_name' =>"Raingad-IM",
     'app_logo' =>"https://im.file.raingad.com/logo/logo.png",
-    'app_version' => '5.1.2',
-    'app_release' =>"20240909",
+    'app_version' => '5.2.1',
+    'app_release' =>"20241113",
     // 应用地址
     'app_host'         => env('app.host', ''),
     // 应用的命名空间
